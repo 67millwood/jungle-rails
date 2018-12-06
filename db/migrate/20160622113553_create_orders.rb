3bad9fc0960711ed1b7bf3c574bacc739a08ecc9
67millwood <info@track2development.com>
2018-12-06 19:15:07 +0000

    initial changes to show order including personalized email and name

--- db/migrate/20160622113553_create_orders.rb
+++ db/migrate/20160622113553_create_orders.rb
@@ -2,7 +2,9 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.integer :total_cents
+      t.references :user, index: true, foreign_key: true
       t.timestamps null: false
+
     end
   end
 end
