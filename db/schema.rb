c97ecc2c032892eb6367c60ec41a4531bb89d2a5
67millwood <info@track2development.com>
2018-12-07 10:35:06 +0000

    reviews tested and working including updated at timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181207095711) do
+ActiveRecord::Schema.define(version: 20181207102600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,10 +59,10 @@ ActiveRecord::Schema.define(version: 20181207095711) do
   create_table "reviews", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "user_id"
-    t.text     "descripion"
+    t.text     "description"
     t.integer  "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
